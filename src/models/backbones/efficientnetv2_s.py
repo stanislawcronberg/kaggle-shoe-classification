f4f2249d470232287be338961a1e82ca572f499c
stanislawcronberg <stanislawcronberg@protonmail.com>
2022-11-09 00:17:50 +0100

    Update efficientnetv2_s.py

--- src/models/backbones/efficientnetv2_s.py
+++ src/models/backbones/efficientnetv2_s.py
@@ -8,6 +8,12 @@ class EffNetV2S(nn.Module):
         assert in_channels == 3, "EfficientNetV2S only supports 3 input channels"
 
         self.model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights.IMAGENET1K_V1)
+
+        # Freeze all layers
+        for child in self.model.children():
+            for param in child.parameters():
+                param.requires_grad = False
+
         self.model.classifier[-1] = nn.Linear(in_features=1280, out_features=n_classes)
 
     def forward(self, x):
