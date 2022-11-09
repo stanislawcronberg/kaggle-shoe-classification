eeebefa5066851638b3d63648a2b3de620016774
stanislawcronberg <stanislawcronberg@protonmail.com>
2022-11-09 18:52:18 +0100

    Update config

--- src/datasets/config.py
+++ src/datasets/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Data:
-    data_dir: str
+    index: dict[str, str]
     image_size: tuple[int, int]
     n_classes: int
     in_channels: int
@@ -19,6 +19,12 @@ class Training:
     trainer_kwargs: dict
     num_workers: int
     pin_memory: bool
+
+
+@dataclass
+class Eval:
+    batch_size: int
+    ckpt_path: str  # Path to model checkpoint
 
 
 @dataclass
