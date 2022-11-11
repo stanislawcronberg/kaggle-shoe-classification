b1dff5d631c62f176947edfd9198831137ae0b78
stanislawcronberg <stanislawcronberg@protonmail.com>
2022-11-11 10:44:58 +0100

    Remove old training from classifier file

--- src/models/shoe_classifier.py
+++ src/models/shoe_classifier.py
@@ -1,24 +1,10 @@
-from pathlib import Path
-
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torchmetrics
 from torch.optim import Adam
-from torchvision.transforms import (
-    ColorJitter,
-    Compose,
-    GaussianBlur,
-    RandomHorizontalFlip,
-    RandomRotation,
-    Resize,
-    ToPILImage,
-    ToTensor,
-)
 
 from datasets.config import ShoeCLFConfig
-from datasets.shoe_dataset import FootwearDataset
-from datasets.utils import get_train_val_dataloaders
 
 
 class ShoeClassifier(pl.LightningModule):
@@ -96,24 +82,3 @@ class ShoeClassifier(pl.LightningModule):
         )
 
         return model
-
-
-if __name__ == "__main__":
-    model = ShoeClassifier()
-    trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=10, precision=16)
-    dataset = FootwearDataset(
-        Path("data/"),
-        transform=Compose(
-            [
-                ToPILImage(),
-                Resize((240, 240)),
-                RandomHorizontalFlip(p=0.5),
-                ColorJitter(brightness=0.5, contrast=0.5, saturation=0.3, hue=0.3),
-                RandomRotation(degrees=10),
-                GaussianBlur(kernel_size=(1, 3), sigma=(0.1, 2.0)),
-                ToTensor(),
-            ]
-        ),
-    )
-    train_dataloader, val_dataloader = get_train_val_dataloaders(dataset)
-    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
