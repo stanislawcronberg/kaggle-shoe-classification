5f10aef130d84767901aa629b4e2038ea9ca7da5
stanislawcronberg <stanislawcronberg@protonmail.com>
2022-11-15 22:14:30 +0100

    Add LightningDataModule

--- src/datasets/shoe_datamodule.py
+++ src/datasets/shoe_datamodule.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import albumentations as A
 import pytorch_lightning as pl
 from albumentations.pytorch import ToTensorV2
-from config import ShoeCLFConfig
-from shoe_dataset import FootwearDataset
 from torch.utils.data import DataLoader
+
+from datasets.config import ShoeCLFConfig
+from datasets.shoe_dataset import FootwearDataset
 
 
 class FootwearDataModule(pl.LightningDataModule):
@@ -13,14 +14,34 @@ class FootwearDataModule(pl.LightningDataModule):
         super().__init__()
         self.cfg = cfg
 
-    def setup(self, stage: str = None):
-        # Initialize transforms
-        self.transforms = self.__initialize_transforms()
+        # * Setup preprocessing and postprocessing transforms
+        # The are transforms that are applied before and after the
+        # augmentation transforms
+        self._pre_transforms: list = [A.Resize(*self.cfg.data.image_size)]
+        self._post_transforms: list = [ToTensorV2()]
 
-        # Initialize datasets
-        self.train_dataset = self.__initialize_dataset(index_path=self.cfg.data.index.train)
-        self.val_dataset = self.__initialize_dataset(index_path=self.cfg.data.index.val)
-        self.test_dataset = self.__initialize_dataset(index_path=self.cfg.data.index.test)
+    def setup(self, stage: str = None):
+
+        if stage == "fit" or stage is None:
+            self._train_transforms = self.__initialize_transforms(
+                use_augmentations=True if self.cfg.training.use_augmentations else False
+            )
+            self._val_transforms = self.__initialize_transforms(use_augmentations=False)
+
+            self.train_dataset = self.__initialize_dataset(self.cfg.data.index.train, self._train_transforms)
+            self.val_dataset = self.__initialize_dataset(self.cfg.data.index.val, self._val_transforms)
+
+        if stage == "test" or stage is None:
+
+            self.eval_transforms = self.__initialize_transforms(use_augmentations=False)
+
+            self.test_dataset = self.__initialize_dataset(self.cfg.data.index.test, transforms=self.eval_transforms)
+            self.test_transforms = self.__initialize_transforms(use_augmentations=False)
+
+        if stage == "predict" or stage is None:
+            # TODO: Decide what to do here, test_dataset is temporary
+            self.predict_transforms = self.__initialize_transforms(use_augmentations=False)
+            self.predict_dataset = self.__initialize_dataset(self.cfg.data.index.test)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, **self.cfg.training.dataloader_kwargs)
@@ -31,18 +52,52 @@ class FootwearDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test_dataset, **self.cfg.eval.dataloader_kwargs)
 
-    def __initialize_dataset(self, index_path: str):
-        # TODO: Consider making root_data_dir part of the config
+    def predict_dataloader(self):
+        # TODO: Decide what to do here, test_dataset is temporary
+        return DataLoader(self.test_dataset, **self.cfg.eval.dataloader_kwargs)
+
+    def __initialize_dataset(self, index_path: str, transforms: A.Compose) -> FootwearDataset:
+        """Initializes the dataset from the index file with the given transforms.
+
+        Args:
+            index_path (str): Path to the index file.
+            transforms (A.Compose): Albumentations Compose object with all transforms.
+
+        Returns:
+            FootwearDataset: Initialized dataset.
+        """
         return FootwearDataset(
-            index_path=index_path,
-            root_data_dir=Path("."),
-            transforms=self.transforms,
+            index_path=Path(index_path),
+            root_data_dir=Path(self.cfg.data.root_data_dir),
+            transforms=transforms,
         )
 
-    def __initialize_transforms(self):
-        return A.Compose(
-            [
-                A.Resize(*self.cfg.data.image_size),
-                ToTensorV2(),
-            ]
-        )
+    def __initialize_transforms(self, use_augmentations: bool) -> A.Compose:
+        """Initialize transforms for albumentations.
+
+        Args:
+            use_augmentations (bool): Whether to use augmentations.
+
+        Returns:
+            A.Compose: Albumentations Compose object with all transforms.
+        """
+        transforms = []
+
+        transforms += self._pre_transforms
+
+        if use_augmentations:
+            transforms += self.__get_augmentations()
+
+        transforms += self._post_transforms
+
+        return A.Compose(transforms)
+
+    def __get_augmentations(self) -> list:
+        """Returns a list of augmentations."""
+        return [
+            A.ColorJitter(brightness=0.4, contrast=0.4, p=0.5),
+            A.Rotate(limit=30, p=0.5),
+            A.GaussianBlur(blur_limit=(3, 5), p=0.5),
+            A.HorizontalFlip(p=0.5),
+            A.CoarseDropout(max_holes=5, max_height=8, max_width=8, p=0.4),
+        ]
--- src/train.py
+++ src/train.py
@@ -1,14 +1,11 @@
 from pathlib import Path
 
-import albumentations as A
 import hydra
 import pytorch_lightning as pl
-from albumentations.pytorch import ToTensorV2
 from lightning_lite.utilities.seed import seed_everything
-from torch.utils.data import DataLoader
 
 from datasets.config import ShoeCLFConfig
-from datasets.shoe_dataset import FootwearDataset
+from datasets.shoe_datamodule import FootwearDataModule
 from models import ShoeClassifier
 
 
@@ -18,34 +15,6 @@ def train(cfg: ShoeCLFConfig) -> None:
     # Set seed
     if cfg.seed:
         seed_everything(cfg.seed)
-
-    # Setup transforms
-    if cfg.training.use_augmentations:
-        transforms = A.Compose(
-            [
-                A.Resize(*cfg.data.image_size),
-                A.ColorJitter(brightness=0.4, contrast=0.4, p=0.5),
-                A.Rotate(limit=30, p=0.5),
-                A.GaussianBlur(blur_limit=(3, 5), p=0.5),
-                A.HorizontalFlip(p=0.5),
-                A.CoarseDropout(max_holes=5, max_height=8, max_width=8, p=0.4),
-                ToTensorV2(),
-            ]
-        )
-    else:
-        transforms = A.Compose([A.Resize(*cfg.data.image_size), ToTensorV2()])
-
-    # Initialize training/validation datasets
-    train_dataset = FootwearDataset(
-        index_path=cfg.data.index.train,
-        root_data_dir=cfg.data.root_data_dir,
-        transforms=transforms,
-    )
-    val_dataset = FootwearDataset(index_path=cfg.data.index.val, root_data_dir=".", transforms=transforms)
-
-    # Initialize training/validation dataloaders
-    train_loader = DataLoader(train_dataset, **cfg.training.dataloader_kwargs)
-    val_loader = DataLoader(val_dataset, **cfg.eval.dataloader_kwargs)
 
     # Setup early stopping
     if cfg.training.use_early_stopping:
@@ -59,7 +28,7 @@ def train(cfg: ShoeCLFConfig) -> None:
     )
 
     # Train model
-    trainer.fit(model, train_loader, val_loader)
+    trainer.fit(model, datamodule=FootwearDataModule(cfg=cfg))
 
 
 if __name__ == "__main__":
